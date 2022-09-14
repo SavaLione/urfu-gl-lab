424ebbca949490c7fff4183b45a68a85cafe418c
Saveliy (SavaLione) Pototskiy <monologuesplus@gmail.com>
2022-09-14 16:28:05 +0500

    first commit

---
+++ src/core/lanthe.cpp
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2022, Savely Pototsky (SavaLione)
+ * All rights reserved.
+ */
+#include "core/lanthe.h"
+
+int main()
+{
+	return 0;
+}
---
+++ src/core/lanthe.h
@@ -0,0 +1,8 @@
+/*
+ * Copyright (c) 2022, Savely Pototsky (SavaLione)
+ * All rights reserved.
+ */
+#ifndef CORE_LANTHE_H
+#define CORE_LANTHE_H
+
+#endif // CORE_LANTHE_H
